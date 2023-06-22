7a91d5e06d23dbfb0e56ea66ec4813458acb6b3c
verygoodtuna <kjy990504@gmail.com>
2023-06-22 18:16:26 +0900

    shinil

--- js/shinil.js
+++ js/shinil.js
@@ -1,5 +1,31 @@
 $(function () {
+    $(window).on('scroll', function(){
+        let sct = $(window).scrollTop();
+        if(sct>0) { 
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+    })
+
     $('.h_slide').slick({
         arrows: false,
+        dots: true,
     })
+
+    $('.main_home .left').on('click', function() {
+        $('.h_slide').slick('slickPrev')
+    })
+
+    $('.main_home .right').on('click', function() {
+        $('.h_slide').slick('slickNext')
+    })
+
+    $('.p_slide').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+    })
+
 })
